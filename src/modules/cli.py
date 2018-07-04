8e976f300ae8607503f6ce81488617331e9dec2c
Cristian Steib <steibkhriz@gmail.com>
2018-07-03 22:00:53 -0300

    fix git path to execute

--- src/modules/cli.py
+++ src/modules/cli.py
@@ -14,12 +14,12 @@ class Cli:
         if self.__show():
             print(msg)
 
-    def warning(self):
-        pass
+    def warning(self, msg):
+        print("[WARNING] : " + msg)
 
     def info(self, msg):
         if self.__show():
-            print(msg)
+            print("[INFO] : " + msg)
 
     def f_info(self, msg):
         if self.full_verbose:
