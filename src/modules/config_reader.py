47972d716103ea6f7a407929fcccd616311567d7
Cristian Steib <cristiansteib@gmail.com>
2018-07-10 13:45:53 -0300

    update

--- src/modules/config_reader.py
+++ src/modules/config_reader.py
@@ -2,30 +2,44 @@
 This module is only used to read the config for to assist the runner .
 
 """
+try:
+    import ConfigParser as configparser
+except ImportError:
+    import configparser
 
 
 class ConfigReader:
 
-    def __init__(self):
-        self.web_port = None
-        self.web_host = None
-        self.url_path = None
-        self.active_web = None
-        self.demonize = None
-        self.demonize_interval = None
-        self.log_level = None
+    def __init__(self, absolute_path):
+        self.settings = configparser.ConfigParser(defaults=self.get_defaults())
+        self.settings.read(absolute_path)
+        self._web_port = None
+        self._web_host = None
+        self._url_path = None
+        self._active_web = None
+        self._demonize = None
+        self._demonize_interval = None
+        self._log_level = None
+        self._process_web_service_config()
+
+    def _process_web_service_config(self):
+        self._web_port = self.settings.get('web','port')
+
+    @staticmethod
+    def get_defaults():
+        return {}
 
     def demonize(self):
         pass
 
     def active_web(self):
-        return self.active_web
+        return self._active_web
 
     def web_port(self):
-        return self.web_port
+        return self._web_port
 
     def web_host(self):
-        return self.web_host
+        return self._web_host
 
     def configs_directory(self):
         """
--- src/modules/web_server.py
+++ src/modules/web_server.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, abort, request
 import logging
 from modules.config_reader import ConfigReader
 from modules.updater import Updater
-
+import json
 app = Flask(
     'WebGitUpdaterService',
 )
@@ -16,19 +16,23 @@ logging.basicConfig(
 )
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    if not request.json:
+        abort(400)
+    data = request.json
+    # todo check json git project name
     updater.run_if_project_exists('unAventon')
-    return 'Hello world'
+    return json.dumps(data)
 
 
 def run(*args, **kwargs):
-    config_instance = ConfigReader()
+    config_instance = ConfigReader('conf/config.ini')
     global updater
     updater = Updater(
         configs_directory=config_instance.configs_directory(),
     )
-    app.run(*args, debug=False, host=config_instance.web_host(), port=config_instance.web_port(), **kwargs)
+    app.run(*args, debug=True, host=config_instance.web_host(), port=config_instance.web_port(), **kwargs)
 
 if __name__ == '__main__':
     run()
